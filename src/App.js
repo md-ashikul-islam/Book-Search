494a74fff4033dcecd09313621067ad7442c2363
md-ashikul-islam <md.ashikul.islam@g.bracu.ac.bd>
2022-05-27 19:06:26 +0600

    v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import useBookSearch from "./useBookSearch";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [pageNumber, setpageNumber] = useState(1);
+
+  function handleSearch(e) {
+    setQuery(e.target.value);
+    setpageNumber(1);
+  }
+
+  useBookSearch(query, pageNumber);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input type="text" onChange={handleSearch}></input>
+      <div>Title</div>
+      <div>Title</div>
+      <div>Title</div>
+      <div>Title</div>
+      <div>Loading...</div>
+      <div>Error</div>
+    </>
   );
 }
 
---
+++ src/useBookSearch.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function useBookSearch(query, pageNumber) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [books, setBooks] = useState([]);
+  const [hasMore, setHasMore] = useState(false);
+  useEffect(() => {
+    let cancel;
+    axios({
+      method: "GET",
+      url: "http://openlibrary.org/search.json",
+      params: { q: query, page: pageNumber },
+      cancelToken: new axios.CancelToken((c) => (cancel = c)),
+    })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((e) => {
+        if (axios.isCancel(e)) return;
+      });
+    return () => cancel();
+  }, [query, pageNumber]);
+  return null;
+}
